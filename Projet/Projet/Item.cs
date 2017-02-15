9bc711b6a6fc7dc2ac813f936639fece40d0b69f
201554580 <201554580@P106-19>
2017-02-15 10:45:07 -0500

    Début de la classe Item

---
+++ Projet/Projet/Item.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna; 
+
+namespace Projet
+{
+    class Item
+    {
+        List<Item> Items { get; set; }
+        string Nom { get; set; }
+        string Catégorie { get; set; }
+
+        public Item(string nom, string catégorie)
+        {
+            Nom = nom;
+            Catégorie = catégorie;
+            Items = new List<Item>();
+        }
+
+        
+        private void CréerItem(string nom, string catégorie)
+        {
+            // Normaliser le nom de la catégorie pour calquer le nom des classes
+            catégorie = Char.ToUpper(catégorie[0]) + catégorie.Substring(1).ToLower();
+
+            // Ajouter le nom du Namespace pour qualifier entièrement
+            catégorie = typeof(Program).Namespace + "." + catégorie;
+
+            // Détermination d'un type en fonction de la chaine 'catégorie'
+            Type typeVoulu = Type.GetType(catégorie);
+
+            // Tentative d'instanciation : le type de la valeur de retour est 'Object'
+            var objetCréé = Activator.CreateInstance(typeVoulu, nom, titre, durée);
+
+            Items.Add(objetCréé as Participant);
+        }
+
+
+    }
+}
