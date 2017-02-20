c33a3a5410e0de0c6b9ced648874e369e847d02c
201561636 <201561636@P106-21>
2017-02-20 12:52:28 -0500

    Modification personnage

--- Projet/Projet/Personnage.cs
+++ Projet/Projet/Personnage.cs
@@ -8,13 +8,29 @@ namespace AtelierXNA
 {
     abstract class Personnage : ObjetDeBase
     {
+        int ptsDeVie;
         public string Nom { get; protected set; }
-        public int PtsDeVie { get; protected set; }
+        public int PtsDeVie
+        {
+            get { return ptsDeVie; }
+            protected set
+            {
+                if (value < 0)
+                {
+                    ptsDeVie = 0;
+                }
+                else ptsDeVie = value;
+            }
+        }
         public int Dextérité { get; protected set; }
         public int Force { get; protected set; }
         public int Intelligence { get; protected set; }
         public int Sagesse { get; protected set; }
         public int PtsDéfense { get; protected set; }
+        public bool EstMort
+        {
+            get { return PtsDeVie == 0; }
+        }
 
         protected Personnage(Game jeu, String nomModèle, float échelleInitiale, Vector3 rotationInitiale, Vector3 positionInitiale, string nom, int force, int dextérité, int intelligence, int sagesse, int ptsDeVie)
             : base(jeu, nomModèle, échelleInitiale, rotationInitiale, positionInitiale)
