71bcdb96d43660d52b373dedb888024bd4ed01db
201554580 <201554580@P106-19>
2017-02-22 11:29:32 -0500

    Item et ItemsManager fonctionnels

--- Projet/Projet/Item.cs
+++ Projet/Projet/Item.cs
@@ -6,37 +6,13 @@ using Microsoft.Xna;
 
 namespace AtelierXNA
 {
-    static class Item
+    class Item
     {
-        static List<Item> Items { get; set; }
         string Nom { get; set; }
-        string Catégorie { get; set; }
 
-        public Item(string nom, string catégorie)
+        public Item(string nom)
         {
             Nom = nom;
-            Catégorie = catégorie;
-            Items = new List<Item>();
         }
-
-        
-        private void CréerItem(string description)
-        {
-        //    // Normaliser le nom de la catégorie pour calquer le nom des classes
-        //    catégorie = Char.ToUpper(catégorie[0]) + catégorie.Substring(1).ToLower();
-
-        //    // Ajouter le nom du Namespace pour qualifier entièrement
-        //    catégorie = typeof(Program).Namespace + "." + catégorie;
-
-        //    // Détermination d'un type en fonction de la chaine 'catégorie'
-        //    Type typeVoulu = Type.GetType(catégorie);
-
-        //    // Tentative d'instanciation : le type de la valeur de retour est 'Object'
-        //    var objetCréé = Activator.CreateInstance(typeVoulu, nom, titre, durée);
-
-        //    Items.Add(objetCréé as Participant);
-        }
-
-
     }
 }
--- Projet/Projet/ItemsManager.cs
+++ Projet/Projet/ItemsManager.cs
@@ -5,7 +5,37 @@ using System.Text;
 
 namespace AtelierXNA
 {
-    class ItemsManager
+    static class ItemsManager
     {
+        static List<Item> Items { get; set; }
+
+        static ItemsManager()
+        {
+            Items = new List<Item>();
+        }
+
+        static void CréerItem(string description)
+        {
+            char séparateur = ';';
+
+            // Extraction du nom et de la catégorie de l'item du string description
+            string[] tableauDescription = description.Split(séparateur);
+            string catégorie = tableauDescription[0];
+            string descriptionRestante = tableauDescription[1];
+
+            // Normaliser le nom de la catégorie pour calquer le nom des classes (Exemple : (J) + (eu) = Jeu)
+            catégorie = Char.ToUpper(catégorie[0]) + catégorie.Substring(1).ToLower();
+
+            // Ajouter le nom du Namespace pour qualifier entièrement
+            catégorie = typeof(Program).Namespace + "." + catégorie;
+
+            // Détermination d'un type en fonction de la chaine 'catégorie'
+            Type typeVoulu = Type.GetType(catégorie);
+
+            // Tentative d'instanciation : le type de la valeur de retour est 'Object'
+            var objetCréé = Activator.CreateInstance(typeVoulu, descriptionRestante);
+
+            Items.Add(objetCréé as Item);
+        }
     }
 }
